f6831ff8a4276c58f635e6164c6300b32d47ab0d
IvoHoffmanns <79205020+IvoHoffmanns@users.noreply.github.com>
2021-03-01 12:53:22 +0100

    Update partialimageright.py

--- partialimageright.py
+++ partialimageright.py
@@ -8,25 +8,20 @@ Created on Mon Feb 22 12:49:21 2021
 # import the necessary packages
 import numpy as np
 import cv2
-from PIL import Image
-from sympy import *
+from PIL import Image		#not currently used
+from sympy import *		#To calculate intersections 
 from sympy.geometry import *
 
-# =============================================================================
-# ap = argparse.ArgumentParser()
-# ap.add_argument("-i", "--image", required = True, help = "Path to the image")
-# args = vars(ap.parse_args())
-# =============================================================================
 
 # load the image, clone it for output, and then convert it to grayscale
 image = cv2.imread('images/test.tiff')
-lbound=475 #x coord of the section
+lbound=475 			#x coord of the section
 imageright=image[:,lbound:]
 outputright = imageright.copy()
 gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
 #Detect lines in the image
-edges = cv2.Canny(gray,6000,6000,apertureSize = 7)
+edges = cv2.Canny(gray,6000,6000,apertureSize = 7)	#ap=7 shows the sharper gradients better
 edgesright=edges[:,lbound:]
 
 # detect circles in the image
@@ -42,12 +37,10 @@ if circles is not None:
 		cv2.circle(outputright, (x, y), r, (0, 255, 0), 1)
 		cv2.rectangle(outputright, (x - 2, y - 2), (x + 2, y + 2), (0, 128, 255), -1)
         
-#cv2.imwrite('houghlines5.jpg',img)
 # show the output image
-cv2.imshow("output", np.hstack([imageright, outputright]))
+cv2.imshow("output", np.hstack([imageright, outputright]))	#show og image and image with drawings next to eachother
 #cv2.imwrite('images/rightsemi.png', np.hstack([imageright, outputright]) ) # save image in images folder
-cright=Circle(Point(x,y),r)
-#cv2.imshow("edges",edges[:,lbound:]) #use this to check the edges detected
-#cv2.imwrite('images/canny50-50.png', edges ) # save image in images folder
+cright=Circle(Point(x,y),r)					#define the circle using the parameters found by HoughCircles
+#cv2.imshow("edges",edges[:,lbound:]) 				#use this to check the edges detected
 cv2.waitKey(0)
-cv2.destroyAllWindows()
+cv2.destroyAllWindows()						#When running, window with image pops up, closes when any key is pressed
